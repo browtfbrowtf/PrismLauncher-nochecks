54df5477e1adc3f81d0eb5341013e6c5913a09e2
browtfbrowtf <placeholder@example.com>
2026-07-16 01:54:02 +0000

    Remove Microsoft account presence checks that gate game launching
    - launcher/LaunchController.cpp: Remove No Accounts dialog, change anyAccountIsValid() to accounts->count() > 0, add early return for offline accounts in decideLaunchMode()
    - launcher/Application.cpp: Remove login variable, login wizard page, unused LoginWizardPage.h include
    - launcher/ui/pages/instance/VersionPage.cpp: Remove download guard for anyAccountIsValid
    - launcher/ui/pages/global/AccountListPage.cpp: Remove offline account creation guard for anyAccountIsValid

--- launcher/Application.cpp
+++ launcher/Application.cpp
@@ -70,7 +70,6 @@
 #include "ui/setupwizard/AutoJavaWizardPage.h"
 #include "ui/setupwizard/JavaWizardPage.h"
 #include "ui/setupwizard/LanguageWizardPage.h"
-#include "ui/setupwizard/LoginWizardPage.h"
 #include "ui/setupwizard/PasteWizardPage.h"
 #include "ui/setupwizard/SetupWizard.h"
 #include "ui/setupwizard/ThemeWizardPage.h"
@@ -1226,9 +1225,8 @@ bool Application::createSetupWizard()
     bool pasteInterventionRequired = settings()->get("PastebinURL") != "";
     bool validWidgets = m_themeManager->isValidApplicationTheme(settings()->get("ApplicationTheme").toString());
     bool validIcons = m_themeManager->isValidIconTheme(settings()->get("IconTheme").toString());
-    bool login = !m_accounts->anyAccountIsValid() && capabilities() & Application::SupportsMSA;
     bool themeInterventionRequired = !validWidgets || !validIcons;
-    bool wizardRequired = javaRequired || languageRequired || pasteInterventionRequired || themeInterventionRequired || askjava || login;
+    bool wizardRequired = javaRequired || languageRequired || pasteInterventionRequired || themeInterventionRequired || askjava;
     if (wizardRequired) {
         // set default theme after going into theme wizard
         if (!validIcons)
@@ -1265,14 +1263,11 @@ bool Application::createSetupWizard()
             m_setupWizard->addPage(new ThemeWizardPage(m_setupWizard));
         }
 
-        if (login) {
-            m_setupWizard->addPage(new LoginWizardPage(m_setupWizard));
-        }
         connect(m_setupWizard, &QDialog::finished, this, &Application::setupWizardFinished);
         m_setupWizard->show();
     }
 
-    return wizardRequired || login;
+    return wizardRequired;
 }
 
 bool Application::updaterEnabled()
